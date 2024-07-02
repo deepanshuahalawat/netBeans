ae45ede71a331188cbea1142ef9797fbfd365f2a
Priyanshu-choudhary <broyadi23@gmail.com>
2024-07-02 15:50:55 +0530

    save

--- src/main/java/org/example/RandomDataGen.java
+++ src/main/java/org/example/RandomDataGen.java
@@ -1,2 +1,21 @@
-package org.example;public class RandomDataGen {
+package org.example;
+
+import java.util.Random;
+
+public class RandomDataGen {
+
+    public  double[]  start(int number) {
+        // Create an array to hold 1000 random double values
+        double[] data = new double[number];
+
+        // Create an instance of Random class
+        Random random = new Random();
+
+        // Fill the array with random double values
+        for (int i = 0; i < data.length; i++) {
+            data[i] = random.nextDouble() * 100; // Random doubles between 0.0 (inclusive) and 100.0 (exclusive)
+        }
+
+        return data;
+    }
 }
